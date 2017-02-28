840ceb5586428965205077c3ac9214b53abb9b60
Timchen7 <timchen7@yahoo.com>
2017-02-28 15:29:14 +0000

    add Wistia video link, prev and next btn

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,10 @@
 class Video < ActiveRecord::Base
+  def next
+    # Video.find(id+1)
+    Video.where("id > ?",id).first
+  end
+  def prev
+    # Video.find(id-1)
+    Video.where("id < ?",id).last
+  end
 end
